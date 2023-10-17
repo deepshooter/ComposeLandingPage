6ceac7c4a66be723af3fd6441879ce433206a87d
avinashmishra <avinashm720@gmail.com>
2023-10-17 21:09:35 +0530

    Initial Commit

---
+++ ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/pages/Index.kt
@@ -0,0 +1,12 @@
+package com.deepshooter.composelandingpage.pages
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.core.Page
+
+@Page
+@Composable
+fun HomePage() {
+    // TODO: Add content here
+    println("Hello World!")
+    println("Test Okay")
+}
