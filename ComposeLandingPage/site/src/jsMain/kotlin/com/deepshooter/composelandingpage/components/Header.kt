62a1d206d5af0b1c3f240bf9021956b203ec9474
avinashmishra <avinashm720@gmail.com>
2023-10-18 19:25:44 +0530

    Add Style and Hover Animations on Header Component

--- ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/components/Header.kt
+++ ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/components/Header.kt
@@ -3,9 +3,12 @@ package com.deepshooter.composelandingpage.components
 import androidx.compose.runtime.Composable
 import com.deepshooter.composelandingpage.models.Section
 import com.deepshooter.composelandingpage.models.Theme
+import com.deepshooter.composelandingpage.styles.LogoStyle
+import com.deepshooter.composelandingpage.styles.NavigationItemStyle
 import com.deepshooter.composelandingpage.util.Constants.FONT_FAMILY
 import com.deepshooter.composelandingpage.util.Res
 import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextDecorationLine
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -13,6 +16,7 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.toModifier
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
@@ -40,6 +44,7 @@ fun LeftSide() {
     Row {
 
         Image(
+            modifier = LogoStyle.toModifier(),
             src = Res.Image.logo,
             desc = "Logo Image"
         )
@@ -63,11 +68,12 @@ fun RightSide() {
         Section.entries.toTypedArray().take(6).forEach { section ->
 
             Link(
-                modifier = Modifier
+                modifier = NavigationItemStyle.toModifier()
                     .padding(right = 30.px)
                     .fontFamily(FONT_FAMILY)
                     .fontSize(18.px)
-                    .fontWeight(FontWeight.Normal),
+                    .fontWeight(FontWeight.Normal)
+                    .textDecorationLine(TextDecorationLine.None),
                 path = section.path,
                 text = section.title
             )
