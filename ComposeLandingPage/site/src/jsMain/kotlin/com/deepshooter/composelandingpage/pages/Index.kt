6fd1e4b69743e472c202497bd957353c613a4469
avinashmishra <avinashm720@gmail.com>
2023-10-18 13:14:25 +0530

    Add git Ignore and Create Index Page

--- ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/pages/Index.kt
+++ ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/pages/Index.kt
@@ -1,12 +1,24 @@
 package com.deepshooter.composelandingpage.pages
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Add content here
-    println("Hello World!")
-    println("Index Page")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+    }
+
 }
