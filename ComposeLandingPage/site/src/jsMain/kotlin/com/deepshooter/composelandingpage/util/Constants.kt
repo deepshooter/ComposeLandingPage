afe09b305900a33644ca9d7846b11b0a95dd9a5b
avinashmishra <avinashm720@gmail.com>
2023-10-19 07:40:39 +0530

    Implement Main Content

--- ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/util/Constants.kt
+++ ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/util/Constants.kt
@@ -7,6 +7,13 @@ object Constants {
     const val FONT_FAMILY = "Roboto"
     const val WEBSITE = "https://github.com/deepshooter"
 
+    const val LOREM_IPSUM_LONG =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    const val LOREM_IPSUM_SHORTEST =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    const val LOREM_IPSUM_SHORT =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+
 }
 
 object Res {
@@ -14,5 +21,6 @@ object Res {
     object Image {
         const val background = "background.svg"
         const val logo = "logo.svg"
+        const val main = "main_image.png"
     }
 }
