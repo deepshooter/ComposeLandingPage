907c6faccedeb96caf2123a3facc386e5cbd466a
avinashmishra <avinashm720@gmail.com>
2023-10-23 07:20:14 +0530

    Implement Portfolio Navigation

--- ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/section/PortfolioSection.kt
+++ ComposeLandingPage/site/src/jsMain/kotlin/com/deepshooter/composelandingpage/section/PortfolioSection.kt
@@ -5,17 +5,25 @@ import com.deepshooter.composelandingpage.components.PortfolioCard
 import com.deepshooter.composelandingpage.components.SectionTitle
 import com.deepshooter.composelandingpage.models.Portfolio
 import com.deepshooter.composelandingpage.models.Section
+import com.deepshooter.composelandingpage.styles.PortfolioArrowIconStyle
 import com.deepshooter.composelandingpage.util.Constants.SECTION_WIDTH
+import com.varabyte.kobweb.compose.css.Cursor
 import com.varabyte.kobweb.compose.css.Overflow
 import com.varabyte.kobweb.compose.css.ScrollBehavior
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.icons.fa.FaArrowLeft
+import com.varabyte.kobweb.silk.components.icons.fa.FaArrowRight
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
 import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import kotlinx.browser.document
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
@@ -56,6 +64,7 @@ fun PortfolioContent() {
             section = Section.Portfolio
         )
         PortfolioCards(breakpoint = breakpoint)
+        PortfolioNavigation()
     }
 
 }
@@ -83,5 +92,33 @@ fun PortfolioCards(breakpoint: Breakpoint) {
                 portfolio = portfolio
             )
         }
+    }
+}
+
+@Composable
+fun PortfolioNavigation() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        FaArrowLeft(
+            modifier = PortfolioArrowIconStyle.toModifier()
+                .margin(right = 40.px)
+                .cursor(Cursor.Pointer)
+                .onClick {
+                    document.getElementById("scrollableContainer")
+                        ?.scrollBy(x = (-325.0), y = 0.0)
+                },
+            size = IconSize.LG
+        )
+        FaArrowRight(
+            modifier = PortfolioArrowIconStyle.toModifier()
+                .cursor(Cursor.Pointer)
+                .onClick {
+                    document.getElementById("scrollableContainer")
+                        ?.scrollBy(x = 325.0, y = 0.0)
+                },
+            size = IconSize.LG
+        )
     }
 }
