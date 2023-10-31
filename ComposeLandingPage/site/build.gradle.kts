7fbbfe8d976a97e6c8c745e755369d15d4cb9446
avinashmishra <avinashm720@gmail.com>
2023-10-31 08:02:15 +0530

    Final Touch

--- ComposeLandingPage/site/build.gradle.kts
+++ ComposeLandingPage/site/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.kobweb.application)
-    alias(libs.plugins.kobwebx.markdown)
+    //alias(libs.plugins.kobwebx.markdown)
 }
 
 group = "com.deepshooter.composelandingpage"
@@ -36,7 +36,7 @@ kobweb {
 }
 
 kotlin {
-    configAsKobwebApplication("composelandingpage", includeServer = true)
+    configAsKobwebApplication("composelandingpage")
 
     sourceSets {
         val commonMain by getting {
@@ -51,13 +51,13 @@ kotlin {
                 implementation(libs.kobweb.core)
                 implementation(libs.kobweb.silk)
                 implementation(libs.silk.icons.fa)
-                implementation(libs.kobwebx.markdown)
+                //implementation(libs.kobwebx.markdown)
             }
         }
-        val jvmMain by getting {
+      /*  val jvmMain by getting {
             dependencies {
                 implementation(libs.kobweb.api)
             }
-        }
+        }*/
     }
 }
